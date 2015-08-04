01ee50cc7858d00d0a0dd66ff9111e85293d9efa
Thomas Schlegel <thomas.schlegel@fullstackfitness.com>
2015-08-03 23:46:54 -0500

    Init

---
+++ Controllers/LeagueController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FantasyMTG.Controllers
+{
+    public class LeagueController : Controller
+    {
+        //
+        // GET: /League/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FantasyMTG.MvcApplication" Language="C#" %>
